a1c6cf8377cb75dad64b9f0b277641673d58871c
Dmitriy Temnov <dmitriy.temnov@webpros.com>
2026-01-07 13:12:24 +0200

    Splitted tests for Graph and ACOService

--- main.cpp
+++ main.cpp
@@ -76,98 +76,58 @@ void testGraphClass()
                 cout << "TEST PASSED: Can't add edge if it already exists between the same vertices (non-directed graph)" << endl;
         }
 
-        // testing pheromone matrix using ACOService
-        graph.addVertex("D");
-        graph.addEdge("B", "D", 3);
-        
-        // Create ACOService from the graph's adjacency matrix
-        map<string, map<string, int>> adjacencyMatrix;
-        vector<string> vertices = graph.getVertices();
-        for (const string& v1 : vertices)
-        {
-                for (const string& v2 : vertices)
-                {
-                        if (graph.edgeExist(v1, v2))
-                        {
-                                adjacencyMatrix[v1][v2] = graph.getWeight(v1, v2);
-                        }
-                        else if (v1 == v2)
-                        {
-                                adjacencyMatrix[v1][v2] = 0;
-                        }
-                }
-        }
-        ACOService acoService(adjacencyMatrix);
-        
-        // Test that pheromone is initialized to 0 when edge is added
-        assert(acoService.getPheromone("B", "D") == 0);
-        
-        // Test that setter sets a value and it can be retrieved by getter
-        const int testPheromone = 42;
-        acoService.setPheromone("B", "D", testPheromone);
-        assert(acoService.getPheromone("B", "D") == testPheromone);
-        
-        // Test that exception is thrown when trying to get pheromone on non-existent edge
-        try
-        {
-                acoService.getPheromone("A", "D");
-        }
-        catch (EdgeNotExist)
-        {
-                cout << "TEST PASSED: Exception thrown when getting pheromone on non-existent edge" << endl;
-        }
-        
-        // Test that exception is thrown when trying to set pheromone on non-existent edge
-        try
-        {
-                acoService.setPheromone("A", "D", 10);
-        }
-        catch (EdgeNotExist)
-        {
-                cout << "TEST PASSED: Exception thrown when setting pheromone on non-existent edge" << endl;
-        }
-
 }
 
-// this function fills a graph adjacency matrix from a file
-// file format: NxN matrix, filled with:
-// * 0 for row=column;
-// * INF if the edge does not exist
-// * edge weight, if edge exists
-
-void readFromFile(const string filename, Graph* graph)
+void testACOServiceClass()
 {
-	ifstream input(filename);
-
-
-	string line;
-	int r_count = 0;
-	int c_count = 0;
-	while(getline(input, line))
+	// ACOSERVICE CLASS TESTS
+	map<string, map<string, int>> adjacencyMatrix;
+	
+	// Create a simple graph: A-B-D, B-C
+	adjacencyMatrix["A"]["A"] = 0;
+	adjacencyMatrix["A"]["B"] = 1;
+	adjacencyMatrix["B"]["A"] = 1;
+	adjacencyMatrix["B"]["B"] = 0;
+	adjacencyMatrix["B"]["C"] = 5;
+	adjacencyMatrix["B"]["D"] = 3;
+	adjacencyMatrix["C"]["B"] = 5;
+	adjacencyMatrix["C"]["C"] = 0;
+	adjacencyMatrix["D"]["B"] = 3;
+	adjacencyMatrix["D"]["D"] = 0;
+	
+	ACOService acoService(adjacencyMatrix);
+	
+	// Test that pheromone is initialized to 0 when edge is added
+	assert(acoService.getPheromone("B", "D") == 0);
+	
+	// Test that setter sets a value and it can be retrieved by getter
+	const int testPheromone = 42;
+	acoService.setPheromone("B", "D", testPheromone);
+	assert(acoService.getPheromone("B", "D") == testPheromone);
+	
+	// Test that exception is thrown when trying to get pheromone on non-existent edge
+	try
 	{
-		string v_name = to_string(r_count);
-		stringstream ss(line);
-	        while(ss.good())
-        	{
-                	string substr;
-                	getline(ss, substr, ',');
-                	graph->addVertex(substr);
-        	}
-
+		acoService.getPheromone("A", "D");
 	}
-	stringstream ss(line);
-	while(ss.good())
+	catch (EdgeNotExist)
 	{
-		string substr;
-		getline(ss, substr, ',');
-		graph->addVertex(substr);
+		cout << "TEST PASSED: Exception thrown when getting pheromone on non-existent edge" << endl;
 	}
-
-	// get edges weights
-		
+	
+	// Test that exception is thrown when trying to set pheromone on non-existent edge
+	try
+	{
+		acoService.setPheromone("A", "D", 10);
+	}
+	catch (EdgeNotExist)
+	{
+		cout << "TEST PASSED: Exception thrown when setting pheromone on non-existent edge" << endl;
+	}
 }
 
 int main()
 {
 	testGraphClass();
+	testACOServiceClass();
 }
